eb5a68695501b5dca679813295958367b7fdcece
DidukhSofia <122630297+DidukhSofia@users.noreply.github.com>
2023-05-20 23:23:01 +0300

    Add files via upload

---
+++ terraform.tfvars
@@ -0,0 +1,10 @@
+aws_access_key = "AKIAZFWW2PGXYVQBQLI2"
+aws_secret_key = "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"
+aws_region = "eu-north-1"  # Замініть на ваш регіон AWS, наприклад, "eu-central-1" для Центральної Європи
+
+vpc_cidr_block = "10.0.0.0/16"
+subnet_cidr_block = "10.0.1.0/24"
+aws_availability_zone = "eu-north-1a"  # Замініть на доступну зону доступності в вашому регіоні
+ec2_ami = "ami-0a79730daaf45078a"
+ec2_instance_type = "t3.micro"
+ec2_key_name = "Pypsuk"
---
+++ variables.tf
@@ -0,0 +1,37 @@
+variable "aws_access_key" {
+  description = "AKIAZFWW2PGXYVQBQLI2"
+}
+
+variable "aws_secret_key" {
+  description = "TzZSH3fCAjvSILEs35lA3b121a0R577tVDwmP0nu"
+}
+
+variable "aws_region" {
+  description = "eu-north-1"
+}
+
+variable "vpc_cidr_block" {
+  description = "10.0.0.0/16"
+}
+
+variable "subnet_cidr_block" {
+  description = "10.0.1.0/24"
+}
+
+variable "aws_availability_zone" {
+  description = "eu-north-1"
+}
+
+variable "ec2_ami" {
+  description = "ami-0a79730daaf45078a"
+}
+
+variable "ec2_instance_type" {
+  description = "t3.micro"
+}
+
+variable "ec2_key_name" {
+  description = "Pypsuk"
+  }
+
+
